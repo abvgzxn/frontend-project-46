10c0d1d641089c0b190eacba2460264be2cc737d
abvgzxn <tryonce499@gmail.com>
2026-03-28 15:08:40 +0300

    style: fix brace-style (catch on separate line)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -14,7 +14,7 @@ program
     try {
       const diff = genDiff(filepath1, filepath2, options.format)
       console.log(diff)
-    } 
+    }
     catch (error) {
       console.error('Error:', error.message)
       process.exit(1)
